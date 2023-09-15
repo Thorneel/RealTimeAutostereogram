59acafeec6ae4fd42859026eef99a8228a3702e4
Elie Thorne <elie_thorne@hotmail.com>
2023-09-16 01:09:36 +0200

    Added a large ball to the test environment for testing

--- Scenes/Ball/ball.gd
+++ Scenes/Ball/ball.gd
@@ -3,6 +3,7 @@ extends RigidBody3D
 
 
 @onready var collision_shape:CollisionShape3D = $CollisionShape3D
+@onready var mesh:MeshInstance3D = $MeshInstance3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,12 +12,19 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
+
+
+func set_radius(radius:float):
+	collision_shape.shape.radius = radius
+	mesh.mesh.radius = radius
+	mesh.mesh.height = radius * 2.
 
 
 func toggle_physics():
 	collision_shape.disabled = !collision_shape.disabled
+
 
 func throw(force:float):
 	var throw_vector:Vector3 = global_transform.basis.z * -1. * force
--- Scenes/TestEnvironment/test_environment.gd
+++ Scenes/TestEnvironment/test_environment.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var ball:ThrowingBall = $Ball
+
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("quit"):
@@ -8,9 +10,12 @@ func _unhandled_input(_event):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	ball.set_radius(1.)
+	ball.set_mass(100.)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+
